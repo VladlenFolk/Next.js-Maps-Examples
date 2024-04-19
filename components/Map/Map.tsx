aebf8362c1a5a03813421b1311f06a9537e0093c
Avok@to34 <vladlen-17@yandex.ru>
2024-04-19 22:34:35 +0300

    decided to delete extra info

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,11 +1,10 @@
 "use client";
 import React, { useEffect, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom";
-import type { YMapLocationRequest } from 'ymaps3';
+import type { YMapLocationRequest } from "ymaps3";
 // import { useDispatch, useSelector } from 'react-redux';
 // import { close, open, getCoordinates, setZoom } from '@/reducers/map/map';
 import CustomMarkerWithPopup from "./CustomMarker/CustomMarker";
-
 
 //Данные, получаемые после загрузки скрипта
 const [ymaps3React] = await Promise.all([
@@ -30,128 +29,10 @@ const { YMapClusterer, clusterByGrid } = reactify.module(
 );
 
 export default function Map() {
-  // const dispatch = useDispatch();
-
-  // const { coordinates, zoom, mapPopupStatus } = useSelector(
-  //   (state) => state.mapModal
-  // );
-
-  //Замаскировал ошибку, возникающую из-за тега YM
-  const realError = console.error;
-  console.error = (...x) => {
-    // debugger;
-    if (
-      x.indexOf(
-        "Warning: The tag <ymaps> is unrecognized in this browser. If you meant to render a React component, start its name with an uppercase letter."
-      )
-    ) {
-      return;
-    }
-    realError(...x);
-  };
-
-  /*При начальной загрузке страницы закрываем все попапы, которые
-   которые могли остаться открытыми
-  */
-  useEffect(() => {
-    closePopup();
-  }, []);
-
-  //Функции открытия/закрытия попапа, которые хранят состояние в store
-  const closePopup = () => {
-    // dispatch(close());
-  };
-  const openPopup = () => {
-    // dispatch(open());
-  };
-
-  /*Функция, которая отслеживает события внутри карты, setZoom и getCoordinates
-  получают значение зума и координат и сохраняют в сторе. В свою очередь из
-  стора мы используем их для установки позиционирования карты YMap location={}
-  при клике на маркер.
-  */
-  const createBehaviorEventHandler = useCallback(() => {
-    return function (object) {
-      if (object.type === "dblClick") return;
-      // dispatch(setZoom(object.location.zoom));
-      // dispatch(getCoordinates(object.location.center));
-      closePopup();
-    };
-  }, []);
-
-  //Функция для кластеризции, чем больше gridSize тем быстрее маркеры схлопнутся
-  const gridSizedMethod = useMemo(() => clusterByGrid({ gridSize: 30 }), []);
-
-  //Функция, в которой перебираем места и добавляем нужные поля
-  const points = places.map((point, i) => ({
-    type: "Feature",
-    id: i,
-    geometry: { coordinates: [point.longitude, point.latitude] },
-    name_fund: point.name_fund,
-    site: point.site,
-    address: point.address,
-    properties: { name: "Point of issue of orders" },
-  }));
-
-  //Функция для создания одиночного маркера
-  const marker = useCallback(
-    (feature) => (
-      <CustomMarkerWithPopup
-        coordinates={feature.geometry.coordinates}
-        popupMapOpen={openPopup}
-        popupMapClose={closePopup}
-        name_fund={feature.name_fund}
-        site={feature.site}
-        address={feature.address}
-        source={"my-source"}
-      />
-    ),
-    []
-  );
-
-  //Функция для создания отдельного кластера
-  const cluster = useCallback((coordinates, features) => {
-    /*Если координат нет (значение == null), то в кластер попадает значение
-    [0,0], поэтому кластер отрисовывался на экваторе))
-    */
-    if (coordinates[0] === 0 || coordinates[1] === 0) {
-      return null;
-    }
-    return (
-      <YMapMarker coordinates={coordinates} source={"my-source"}>
-        <div
-          className="circle"
-          style={
-            mapPopupStatus ? { display: "none" } : { display: "inline-block" }
-          }
-        >
-          <div className="circle-content">
-            <span className="circle-text">{features.length}</span>
-          </div>
-        </div>
-      </YMapMarker>
-    );
-  }, []);
-
   return (
-    <YMap location = {{ center: coordinates, zoom: zoom, duration: 500 }}>
+    <YMap location={}>
       <YMapDefaultSchemeLayer />
-      <YMapListener
-        onActionEnd={useMemo(() => createBehaviorEventHandler(), [])}
-      />
       <YMapDefaultFeaturesLayer />
-      {
-        <>
-          <YMapFeatureDataSource id="my-source" />
-          <YMapLayer source="my-source" type="markers" zIndex={1800} />
-          <YMapClusterer
-            marker={marker}
-            cluster={cluster}
-            method={gridSizedMethod}
-            features={points}
-          />
-        </>
-      }
     </YMap>
   );
 }
