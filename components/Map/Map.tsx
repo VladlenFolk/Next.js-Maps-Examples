8caa6ce464d9fc845062bef34e12f3e966517f3d
Avok@to34 <vladlen-17@yandex.ru>
2024-04-27 23:43:37 +0300

    feat: added marker

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom";
 import type { YMapLocationRequest } from "ymaps3";
 import { LOCATION } from "@/utils/constants";
-import style from '@/components/Map/Map.module.scss'
+import style from "@/components/Map/Map.module.scss";
+import Image from "next/image";
 
 //Данные, получаемые после загрузки скрипта
 const [ymaps3React] = await Promise.all([
@@ -21,13 +22,20 @@ const {
   YMapFeatureDataSource,
 } = reactify.module(ymaps3);
 
-
 export default function Map() {
   return (
     <div className={style.mapContainer}>
       <YMap location={LOCATION}>
         <YMapDefaultSchemeLayer />
         <YMapDefaultFeaturesLayer />
+        <YMapMarker coordinates={[37.504314, 55.73025]}>
+          <Image
+            src={"/blue-marker.png"}
+            alt={"museum"}
+            width={50}
+            height={50}
+          ></Image>
+        </YMapMarker>
       </YMap>
     </div>
   );
