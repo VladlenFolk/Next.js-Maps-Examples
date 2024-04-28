ca0eae225d689db31929422d828ca7270f9893f4
Avok@to34 <vladlen-17@yandex.ru>
2024-04-28 16:36:40 +0300

    feat: added info about museums

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom";
 import type { YMapLocationRequest } from "ymaps3";
 import { LOCATION } from "@/utils/constants";
 import style from "@/components/Map/Map.module.scss";
-import Image from "next/image";
 import CustomMarkerWithPopup from "@/components/YMapMarker/YMapMarker";
+import { museum } from "@/utils/places";
 
 //Данные, получаемые после загрузки скрипта
 const [ymaps3React] = await Promise.all([
@@ -23,13 +23,32 @@ const {
   YMapFeatureDataSource,
 } = reactify.module(ymaps3);
 
+const { YMapClusterer, clusterByGrid } = reactify.module(
+  await ymaps3.import("@yandex/ymaps3-clusterer@0.0.1")
+);
+
 export default function Map() {
+
+  //Функция для кластеризции, чем больше gridSize тем быстрее маркеры схлопнутся
+  const gridSizedMethod = useMemo(() => clusterByGrid({ gridSize: 64 }), []);
+
+    //Функция, в которой перебираем места и добавляем нужные поля
+    const ceatePoints = places.map((point, i) => ({
+      type: 'Feature',
+      id: i,
+      geometry: { coordinates: [point.longitude, point.latitude] },
+      name_fund: point.name_fund,
+      site: point.site,
+      address: point.address,
+      properties: { name: 'Point of issue of orders' }
+    }));
+
   return (
     <div className={style.mapContainer}>
       <YMap location={LOCATION}>
         <YMapDefaultSchemeLayer />
         <YMapDefaultFeaturesLayer />
-        <CustomMarkerWithPopup coordinates={[37.504062,55.730588]} />
+        <CustomMarkerWithPopup coordinates={[37.504062, 55.730588]} />
       </YMap>
     </div>
   );
---
+++ utils/places.ts
@@ -0,0 +1,26 @@
+export const museum = [
+    {
+        id:1,
+        name: 'Музей победы',
+        site: 'https://victorymuseum.ru/',
+        coordinates: [37.504062, 55.730588]
+    },
+    {
+        id:2,
+        name: 'Музей Гоголя',
+        site: 'https://www.domgogolya.ru/',
+        coordinates: [37.599861, 55.754084]
+    },
+    {
+        id:3,
+        name: 'Музей архитектуры',
+        site: 'https://muar.ru/',
+        coordinates: [37.607368, 55.752481]
+    },
+    {
+        id:4,
+        name: 'Бородинская битва',
+        site: 'https://1812panorama.ru/',
+        coordinates: [37.523187, 55.738752]
+    }
+]
